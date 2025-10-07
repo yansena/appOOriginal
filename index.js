b38bf40ad358d801f95fb6b3174d9758665b4128
Yanderson Sena <contato@yandev.com.br>
2025-10-07 12:13:44 -0300

    Revert "Update index.js"

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-
 import App from './src/App';
-
 import {name as appName} from './app.json';
-
 import './gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
